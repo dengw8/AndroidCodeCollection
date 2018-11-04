c6a54e5b35dc0e8dfdb11edb2eff10e9d8080da1
dengwang <dengwang@bytedance.com>
2018-11-04 12:39:12 +0800

    add Modularization demo

---
+++ 组件化/Android-Router/app/src/main/java/com/joybar/androidrouter/App.java
@@ -0,0 +1,90 @@
+package com.joybar.androidrouter;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.joybar.compiler.RouterInject;
+import com.joybar.librouter.Router;
+import com.joybar.librouter.Rule;
+import com.joybar.modulebase.application.ApplicationService;
+import com.joybar.moduleshop.application.ShopApplication;
+import com.joybar.moduleuser.application.UserReleaseApplication;
+import com.me.obo.routerguider.RouterGuider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by obo on 2017/11/28.
+ */
+
+public class App extends Application implements ApplicationService {
+
+    private static App INSTANCE = null;
+    private static final String TAG = "App";
+
+    public static App getInstance() {
+        Log.d(TAG,"get application");
+        return INSTANCE;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        INSTANCE = this;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RouterGuider.inject(this);
+         initRouterByAnnotation();
+        // OR
+        // initRouterByDynamic();
+
+        loadModuleApplicationService();
+    }
+
+
+    private void initRouterByAnnotation() {
+        RouterInject.inject("com.joybar.moduleuser.MainActivity");
+        RouterInject.inject("com.joybar.moduleshop.MainActivity");
+        RouterInject.inject("com.joybar.moduleshop.ReceiveParamActivity");
+        RouterInject.inject("com.joybar.moduleshop.FinishWithResultActivity");
+        RouterInject.inject("com.joybar.moduleshop.PostModuleDataActivity");
+    }
+
+    private void initRouterByDynamic() {
+        Router.registerRouters(new Router.RouterTable() {
+            @Override
+            public List<Rule> buildRuleList() {
+                List<Rule> ruleList = new ArrayList<>();
+                ruleList.add(new Rule("user", "main", com.joybar.moduleuser.MainActivity
+                        .class));
+                ruleList.add(new Rule("shop", "main", com.joybar.moduleshop.MainActivity
+                        .class));
+                ruleList.add(new Rule("shop", "receive_param", com.joybar.moduleshop
+                        .ReceiveParamActivity.class));
+                ruleList.add(new Rule("shop", "finish_with_result", com.joybar.moduleshop
+                        .FinishWithResultActivity.class));
+                ruleList.add(new Rule("shop", "post_module_data", com.joybar.moduleshop
+                        .PostModuleDataActivity.class));
+                return ruleList;
+            }
+        });
+    }
+
+
+    @Override
+    public void loadModuleApplicationService() {
+        UserReleaseApplication.getInstance().loadModuleApplicationService();
+        ShopApplication.getInstance().loadModuleApplicationService();
+    }
+
+    @Override
+    public Application getApplication() {
+        return getInstance();
+    }
+
+}
